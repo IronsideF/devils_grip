0a7287e36601cd19cca53bc720bf250ccfa913fc
James <james.m.linklater@gmail.com>
2022-08-20 11:51:20 +0100

    setup template for game logic

--- client/src/components/GameLogic.js
+++ client/src/components/GameLogic.js
@@ -1,76 +1,119 @@
 import React, { useState } from "react";
 
 const GameLogic = () => {
-    const card1 = { 
-        code: "8C",
-        value: "8",
-        suit: "CLUBS"
-    }
-
-    const card2 = { 
-        code: "JC",
-        value: "JACK",
-        suit: "CLUBS"
-    }
-
-    const card3 = { 
-        code: "2C",
-        value: "2",
-        suit: "CLUBS"},
-
-    const card4 = { 
-        code: "5H",
-        value: "5",
-        suit: "HEARTS"}
-  ;
+  const [cardtop, setCardTop] = useState("");
+  const [cardbot, setCardBot] = useState("");
 
   //giving the card an int value instead of a string one, face cards becom 11, 12, 13 for jack, queen, and king respectively
   const valueConverter = (card) => {
-    switch(card) {
-        case card.value === "2":
-            card.value = 2;
-            break;
-        case card.value === "3":
-            card.value = 3;
-            break;
-        case card.value === "4":
-            card.value = 4;
-            break;
-        case card.value === "5":
-            card.value = 5;
-            break;
-        case card.value === "6":
-            card.value = 6;
-            break;
-        case card.value === "7":
-            card.value = 7;
-            break;
-        case card.value === "8":
-            card.value = 8;
-            break;
-        case card.value === "9":
-            card.value = 9;
-            break;
-        case card.value === "10":
-            card.value = 10;
-            break;
-        case card.value === "11":
-            card.value = 11;
-            break;
-        case card.value === "12":
-            card.value = 12;
-            break;
-        case card.value === "13":
-            card.value = 13;
-            break;
+    switch (card.value) {
+      case "2":
+        card.value = 2;
+        break;
+      case "3":
+        card.value = 3;
+        break;
+      case "4":
+        card.value = 4;
+        break;
+      case "5":
+        card.value = 5;
+        break;
+      case "6":
+        card.value = 6;
+        break;
+      case "7":
+        card.value = 7;
+        break;
+      case "8":
+        card.value = 8;
+        break;
+      case "9":
+        card.value = 9;
+        break;
+      case "10":
+        card.value = 10;
+        break;
+      case "JACK":
+        card.value = 11;
+        break;
+      case "QUEEN":
+        card.value = 12;
+        break;
+      case "KING":
+        card.value = 13;
+        break;
     }
-  }
+  };
 
-  const checkIfStackable = (card1, card2) => {
-    if ()
-  }
+  //the first card passed will be stacked onto the 2nd card passed
+  const checkIfStackable = (event, cardtop, cardbot) => {
+    valueConverter(cardtop);
+    valueConverter(cardbot);
+    if (cardtop.suit === cardbot.suit) {
+      if (cardtop.value === cardbot.value + 3) {
+        console.log("stackable");
+      } else {
+        console.log("not stackable");
+      }
+    } else {
+      console.log("not stackable");
+    }
+  };
 
-  return <div>GameLogic</div>;
+  const setCard = (event, card) => {
+    if (cardtop === "") {
+      setCardTop(card);
+      console.log(cardtop);
+    } else if (cardbot === "") {
+      setCardBot(card);
+    } else {
+      console.log("no free slots");
+    }
+  };
+
+  return (
+    <div>
+      <button
+        onClick={(event) =>
+          setCard(event, {
+            code: "5C",
+            value: "5",
+            suit: "CLUBS",
+          })
+        }
+      >
+        Card1 5c
+      </button>
+      <button
+        onClick={(event) =>
+          setCard(event, {
+            code: "2C",
+            value: "2",
+            suit: "CLUBS",
+          })
+        }
+      >
+        Card2 2c
+      </button>
+      <button
+        onClick={(event) =>
+          setCard(event, {
+            code: "2H",
+            value: "2",
+            suit: "HEARTS",
+          })
+        }
+      >
+        Card3 2h
+      </button>
+      <button
+        onClick={(event) => checkIfStackable(event, cardtop, cardbot)}
+      >
+        check if stackable
+      </button>
+    </div>
+  );
 };
 
 export default GameLogic;
