f7838ffbfccc95770d008c6c5d51efddc1d4fd4c
James <james.m.linklater@gmail.com>
2022-08-23 15:13:04 +0100

    added easy mode

--- client/src/containers/DevilContainer.js
+++ client/src/containers/DevilContainer.js
@@ -30,6 +30,7 @@ const DevilContainer = () => {
 	const [cardArrays, setCardArrays] = useState(null);
 	const [gameOver, setGameOver] = useState(false);
 	const [selectedCard, setSelectedCard] = useState(null);
+	const [difficulty, setDifficulty] = useState('normal')
 
 	const deckUrl =
 		"https://www.deckofcardsapi.com/api/deck/new/shuffle/?cards=2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH,2S,3S,4S,5S,6S,7S,8S,9S,0S,JS,QS,KS,2C,3C,4C,5C,6C,7C,8C,9C,0C,JC,QC,KC,2D,3D,4D,5D,6D,7D,8D,9D,0D,JD,QD,KD,2H,3H,4H,5H,6H,7H,8H,9H,0H,JH,QH,KH";
@@ -173,6 +174,7 @@ const DevilContainer = () => {
 		setCardArrays(null);
 		setSelectedCard(null);
 		setGameOver(true);
+		setDifficulty('normal')
 	};
 
 	const exitGameOver = () => {
@@ -192,7 +194,7 @@ const DevilContainer = () => {
 		if (!(cardBotX === "") && !cardTopX.code) {
 			const canStack = checkIfStackable(
 				gridCards[cardTopX][cardTopY],
-				gridCards[cardBotX][cardBotY]
+				gridCards[cardBotX][cardBotY], difficulty
 			);
 			if (canStack) {
 				stackGridCards();
@@ -212,7 +214,7 @@ const DevilContainer = () => {
 		} else if (!(cardBotX === "")) {
 			const canStack = checkIfStackable(
 				[cardTopX],
-				gridCards[cardBotX][cardBotY]
+				gridCards[cardBotX][cardBotY], difficulty
 			);
 			if (canStack) {
 				stackFromTalon();
@@ -243,6 +245,10 @@ const DevilContainer = () => {
 		}
 		return sortedScores;
 	};
+
+	const changeDifficulty = (event) => {
+		setDifficulty(event.target.value);
+	}
 
 	useEffect(() => {
 		if (gridCards) {
@@ -283,7 +289,15 @@ const DevilContainer = () => {
 							/>
 						</>
 					) : (
+						<>
 						<PlayButton getDeck={getDeck} />
+						<label for="difficulty" >Choose a difficulty</label>
+						<select name="difficulty" id="difficulty" onChange={changeDifficulty}>
+							<option value="normal" selected>Normal Rules</option>
+							<option value="colours">Matching Colours</option>
+							
+						</select>
+						</>
 					)}
 					{deck ? (
 						<StockSection
