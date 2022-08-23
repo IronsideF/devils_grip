80bf61ae96c6ee03f6b308e1d0bce7e9f072c7f1
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-08-23 13:49:07 +0100

    implemented helper node which displays rules on hover

--- client/src/components/HelperNode.js
+++ client/src/components/HelperNode.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const HelperNode = () => {
   return (
-    <div>HelperNode</div>
+    <HintButton>❓<HintInfo>Stack Rules<br/>2,5,8,J<br/>3,6,9,Q<br/>4,7,10,K</HintInfo></HintButton>
   )
 }
+
+const HintButton = styled.div`
+  background-color: #AFAFAF;
+  border: solid black 3px;
+  border-radius: 50px;
+  height: 3vh;
+  width: 2vw;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+`;
+
+const HintInfo = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #AFAFAF;
+  border: solid black 3px;
+  border-radius: 5px;
+  opacity: 0.9;
+  padding: 1%;
+    ${HintButton}:hover & {
+        display: block;
+    }
+`;
 
 export default HelperNode
