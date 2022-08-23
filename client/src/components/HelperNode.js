d0431e538da07eb7dc343b5b41eb03725a15c4a8
ScottDrysdale86 <sdrysdale86@pm.me>
2022-08-23 14:54:01 +0100

    css for leaderboard and helper

--- client/src/components/HelperNode.js
+++ client/src/components/HelperNode.js
@@ -1,34 +1,47 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const HelperNode = () => {
-  return (
-    <HintButton>❓<HintInfo>Stack Rules<br/>2,5,8,J<br/>3,6,9,Q<br/>4,7,10,K</HintInfo></HintButton>
-  )
-}
+	return (
+		<HintButton>
+			❓
+			<HintInfo>
+				Stack Rules
+				<br />
+				2,5,8,J
+				<br />
+				3,6,9,Q
+				<br />
+				4,7,10,K
+			</HintInfo>
+		</HintButton>
+	);
+};
 
 const HintButton = styled.div`
-  background-color: #AFAFAF;
-  border: solid black 3px;
-  border-radius: 50px;
-  height: 3vh;
-  width: 2vw;
-  display: flex;
-  justify-content: center;
-  align-items:center;
+	background-color: #afafaf;
+	border: solid black 3px;
+	border-radius: 50px;
+	height: 3vh;
+	width: 2vw;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
 
 const HintInfo = styled.div`
-  display: none;
-  position: absolute;
-  background-color: #AFAFAF;
-  border: solid black 3px;
-  border-radius: 5px;
-  opacity: 0.9;
-  padding: 1%;
-    ${HintButton}:hover & {
-        display: block;
-    }
+	text-align: center;
+	font-weight: bold;
+	display: none;
+	position: absolute;
+	background-color: #afafaf;
+	border: solid black 3px;
+	border-radius: 5px;
+	opacity: 0.9;
+	padding: 1%;
+	${HintButton}:hover & {
+		display: block;
+	}
 `;
 
-export default HelperNode
+export default HelperNode;
