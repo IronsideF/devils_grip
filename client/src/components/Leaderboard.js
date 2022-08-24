ccc914c98e500cf57c04905ba0613b1b765c6c50
James <james.m.linklater@gmail.com>
2022-08-24 11:28:35 +0100

    added a filter to the leaderboard by difficulty

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import LeaderboardItem from "./LeaderboardItem.js";
 import styled from "styled-components";
 
 const Leaderboard = ({ scores }) => {
-	const leaderboardItem = scores.map((score, index) => {
+
+	const [filterDifficulty, setFilterDifficulty] = useState('all')
+
+	const changeFilter = (event) => {
+		setFilterDifficulty(event.target.value);
+	}
+	const leaderboardAll = scores.map((score, index) => {
+
 		return (
 			<LeaderboardItem
 				name={score.name}
@@ -11,16 +18,47 @@ const Leaderboard = ({ scores }) => {
 				key={index}
 				difficulty={score.difficulty}
 			/>
-		);
-	});
+
+		);})
+
+		const normals = scores.filter(score => score.difficulty === "normal" );
+		const leaderboardNormal = normals.map((score, index) => {
+			return (
+				<LeaderboardItem
+				name={score.name}
+				score={score.score}
+				key={index}
+				difficulty={score.difficulty}
+			/>
+			)
+		})
+
+		const colours = scores.filter(score => score.difficulty === "colours" );
+		const leaderboardColours = colours.map((score, index) => {
+			return (
+				<LeaderboardItem
+				name={score.name}
+				score={score.score}
+				key={index}
+				difficulty={score.difficulty}
+			/>
+			)
+		})
 
 	return (
 		<Wrapper>
 			<Title>
 				<u>Devil's Grip Leaderboard</u>
 			</Title>
+			<label htmlFor="filter">Choose a Difficulty: </label>
+			<select name="filter" id ="filter" onChange={changeFilter}>
+				<option defaultValue="all" value="all">All</option>
+				<option value="normal">Normal</option>
+				<option value="colours">Matching Colours</option>
+
+			</select>
 			<Title>Player Name - Score - Difficulty</Title>
-			<List>{leaderboardItem}</List>
+			{ filterDifficulty === "all" ? <List>{leaderboardAll}</List> : filterDifficulty === "normal" ? <List>{leaderboardNormal}</List> : filterDifficulty === "colours" ? <List>{leaderboardColours}</List> : <p>npotjing</p>}
 		</Wrapper>
 	);
 };
